80038ae1d6e70afb6717adb3f335702ef50ed31a
lla922 <104268651+lla922@users.noreply.github.com>
2022-10-01 18:09:01 +0300

    added last work

---
+++ odev3.sql
@@ -0,0 +1,13 @@
+SELECT * FROM country WHERE country LIKE 'A%a';
+
+SELECT * FROM country WHERE country LIKE '______%n';
+
+SELECT * FROM film WHERE title ILIKE '%t%';
+
+SELECT * FROM film WHERE title ILIKE 'C%' AND LENGTH >90 AND rental_rate = 2.99;
+
+
+
+
+
+
